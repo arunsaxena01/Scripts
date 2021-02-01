416b60ed5d71d863e453fec2f04c849d756be658
devopsbc01 <70231154+devopsbc01@users.noreply.github.com>
2021-01-31 19:37:11 -0800

    Update sonar.sh

--- sonar.sh
+++ sonar.sh
@@ -1,4 +1,5 @@
 sudo apt update
+sudo apt-get upgrade
 wget https://raw.githubusercontent.com/devopsbc01/Scripts/master/docker-compose.yml
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
